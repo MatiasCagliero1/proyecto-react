17ec730072586a2f13cb8ee67eb376ba9a8c25af
milagrosechague4 <mechague@udesa.edu.ar>
2021-09-14 15:19:48 -0300

    Avance

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <React.Fragment>
-      {/* Pre Cargar Página */}
-      {/* var loadpage = document.querySelector(".loadpage");
-      loadpage.innerHTML += `<span class="spin" uk-spinner="ratio: 10"></span><h2 class"loadh2">Cargando Pagina</h2>`
-      var allbody = document.querySelector(".totalbody");
-      allbody.style.display="none"; */}
-      <div className="loadpage"></div>
       <Header/>
       <Carrusel/>
       <FilaTarjetas/>
