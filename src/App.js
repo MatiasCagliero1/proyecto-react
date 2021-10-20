f8a9a2f527969e473b8377b94511e9a15993ebfe
Matias C <matiascagliero1@hotmail.com>
2021-10-20 11:26:50 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//  Importamos todos los componentes necesarios
+import React from 'react';
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Main/>
+      <Footer/>
+    </React.Fragment>
+
+    
+  );}
+
+
+  
+export default App;
